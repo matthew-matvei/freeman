701ad4af0714cce915e3163aae5628ca4126addc
Matthew James <matthew.d.james87@gmail.com>
2017-11-11 21:56:29 +0000

    Added basic Status component

---
+++ src/components/panels/Status.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+class Status extends React.Component<{}, {}> {
+    public render(): JSX.Element {
+        return <div>WIP</div>;
+    }
+}
+
+export default Status;
