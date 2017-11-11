3d6bdffc952414182514d447b259248e23845bae
Matthew James <matthew.d.james87@gmail.com>
2017-11-11 18:40:11 +0000

    Added initial components

---
+++ scripts/components/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+class App extends React.Component<{}, {}> {
+    public render(): JSX.Element {
+        return <h1>Hello world!</h1>;
+    }
+}
+
+export default App;
---
+++ scripts/main.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { App } from "components";
+
+ReactDOM.render(<App />, document.getElementById("app-root"));
