c2b3be6367c9428c498280d3f4399a096ea654e1
Matthew James <matthew.d.james87@gmail.com>
2017-11-11 22:24:32 +0000

    Added commenting

--- src/components/panels/Status.tsx
+++ src/components/panels/Status.tsx
@@ -2,7 +2,16 @@ import * as React from "react";
 
 import "styles/panels/Status.scss";
 
+/**
+ * The status indication component
+ */
 class Status extends React.Component<{}, {}> {
+
+    /**
+     * Defines how the status indication component is rendered
+     *
+     * @returns - a JSX element representing the status indication view
+     */
     public render(): JSX.Element {
         return <div className="Status">WIP</div>;
     }
