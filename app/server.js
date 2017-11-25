9ae8263054adddf5c4b98177ddb82edea9e9ca2c
Matthew James <matthew.d.james87@gmail.com>
2017-11-25 12:02:03 +0000

    Added some error handling as per devtron lint

--- app/server.js
+++ app/server.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, dialog } = require("electron");
 require("electron-debug")();
 
 let mainWindow;
@@ -12,11 +12,34 @@ app.on("window-all-closed", () => {
 app.on("ready", () => {
     mainWindow = new BrowserWindow({ width: 1400, height: 800 });
     mainWindow.loadURL(`file://${__dirname}/index.html`);
+
     mainWindow.once("ready-to-show", () => {
         mainWindow.show()
     });
+
     mainWindow.on("closed", () => {
         mainWindow = null;
+    });
+
+    mainWindow.on("unresponsive", () => {
+        const killIndex = 0;
+        const cancelIndex = 1;
+        const kill = dialog.showMessageBox(mainWindow, {
+            type: "warning",
+            buttons: ["OK", "Wait"],
+            defaultId: killIndex,
+            cancelId: cancelIndex,
+            title: "FreeMAN unresponsive",
+            message: "Would you like to kill the process?"
+        });
+    });
+
+    mainWindow.webContents.on("crashed", (event, killed) => {
+        if (killed) {
+            return;
+        }
+
+        dialog.showErrorBox("FreeMAN crashed", event);
     });
 
     // mainWindow.setMenu(null);
