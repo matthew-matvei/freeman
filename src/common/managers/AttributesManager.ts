4d529810860575c63602ff72eb88a4af9e80796d
BISTECH\mjames <mjames@bistech.co.uk>
2018-03-05 08:18:48 +0000

    Importing fswin from correct module

--- src/common/managers/AttributesManager.ts
+++ src/common/managers/AttributesManager.ts
@@ -1,3 +1,4 @@
+import * as fswin from "electron-fswin";
 import { injectable } from "inversify";
 
 import LoggedError from "errors/LoggedError";
@@ -8,16 +9,14 @@ import { IAttributes } from "models";
 @injectable()
 class AttributesManager implements IAttributesManager {
 
-    /**  @inheritDoc */
+    /** @inheritDoc */
     public async getAttributesAsync(pathToItem: string): Promise<IAttributes> {
-        const fswin = require("fswin");
-
         if (!fswin) {
-            throw new LoggedError("Could not import fswin module");
+            throw new LoggedError("Could not import electron-fswin module");
         }
 
         return new Promise<IAttributes>((resolve, reject) => {
-            fswin.getAttributes(pathToItem, (result: any) => {
+            fswin.getAttributes(pathToItem, result => {
                 if (!result) {
                     reject(`Could not retrieve attributes for ${pathToItem}`);
                 }
