0647ad23933e1af0789296b5945e4553757acf65
Matthew James <matthew.d.james87@gmail.com>
2018-03-24 22:41:15 +0000

    Added documentation

--- src/main/arguments/ArgumentsParser.ts
+++ src/main/arguments/ArgumentsParser.ts
@@ -1,6 +1,15 @@
 import { IParsedArguments } from "arguments";
 
+/** Provides a static method for parsing command-line arguments. */
 export default {
+
+    /**
+     * Parses command-line arguments
+     *
+     * @param args - the list of arguments to parse
+     *
+     * @returns a parsed object of command-line arguments
+     */
     parse(args: string[]): IParsedArguments {
         const result: IParsedArguments = {
             verbose: false,
@@ -19,10 +28,24 @@ export default {
     }
 };
 
+/**
+ * Returns whether the given arg identifies that the user has requested verbose mode
+ *
+ * @param arg - the argument to interpret
+ *
+ * @returns whether the given arg identifies that the user has requested verbose mode
+ */
 function verboseRequested(arg: string): boolean {
     return arg === "--verbose" || arg === "-V";
 }
 
+/**
+ * Returns whether the given arg identifies that the user has requested version be shown
+ *
+ * @param arg - the argument to interpret
+ *
+ * @returns whether the given arg identifies that the user has requested version be shown
+ */
 function versionRequested(arg: string): boolean {
     return arg === "--version" || arg === "-v";
 }
