e530c9b1f26104747eb1ce2440cc99e502bd72d4
Matthew James <matthew.d.james87@gmail.com>
2018-04-06 20:49:46 +0100

    Added initial tests for integrated terminal

--- test/renderer/objects/IntegratedTerminal.test.ts
+++ test/renderer/objects/IntegratedTerminal.test.ts
@@ -1,8 +1,53 @@
 import "reflect-metadata";
+import { IMock, Mock, Times } from "typemoq";
+
+import { ISettingsManager } from "managers";
+import { ICommonSettings } from "models/settings";
+import { IntegratedTerminal, IShell } from "objects";
+import applicationSettings from "settings/internal/settings";
 
 describe("IntegratedTerminal's", () => {
+
+    let useFallbackShell: boolean;
+    let settingsManager: IMock<ISettingsManager>;
+    let shell: IMock<IShell>;
+    const userLinuxShell = "/some/custom/shell";
+    const userWindowsShell = "CustomShell.exe";
+    const originalPlatform = process.platform;
+    let userSettings: ICommonSettings;
+
+    before(() => {
+        settingsManager = Mock.ofType<ISettingsManager>();
+        userSettings = JSON.parse(JSON.stringify(applicationSettings));
+        userSettings.linux.shell = userLinuxShell;
+        userSettings.windows.shell = userWindowsShell;
+    });
+
+    beforeEach(() => {
+        useFallbackShell = false;
+        shell = Mock.ofType<IShell>();
+    });
+
+    afterEach(() => {
+        Object.defineProperty(process, "platform", { value: originalPlatform });
+    });
+
     describe("constructor", () => {
-        it("attempts to use configured shell when 'useFallbackShell' is false");
+        it("attempts to use configured shell when 'useFallbackShell' is false on Linux", () => {
+            Object.defineProperty(process, "platform", { value: "linux" });
+
+            new IntegratedTerminal(settingsManager.object, shell.object, useFallbackShell);
+
+            shell.verify(sh => sh.spawn(userLinuxShell), Times.once());
+        });
+
+        it("attempts to use configured shell when 'useFallbackShell' is false on Windows", () => {
+            Object.defineProperty(process, "platform", { value: "win32" });
+
+            new IntegratedTerminal(settingsManager.object, shell.object, useFallbackShell);
+
+            shell.verify(sh => sh.spawn(userWindowsShell), Times.once());
+        });
 
         it("uses fallback shell when 'useFallbackShell' is true");
 
