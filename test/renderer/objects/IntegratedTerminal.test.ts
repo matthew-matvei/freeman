f4a87676d5e9d5f5d9975d593a11afd17cffd4f1
Matthew James <matthew.d.james87@gmail.com>
2018-04-06 21:30:31 +0100

    Implemented all-but-one tests for integrated terminal

--- test/renderer/objects/IntegratedTerminal.test.ts
+++ test/renderer/objects/IntegratedTerminal.test.ts
@@ -1,5 +1,7 @@
+import { expect } from "chai";
+import os from "os";
 import "reflect-metadata";
-import { IMock, Mock, Times } from "typemoq";
+import { IMock, It, Mock, Times } from "typemoq";
 
 import { ISettingsManager } from "managers";
 import { ICommonSettings } from "models/settings";
@@ -14,6 +16,7 @@ describe("IntegratedTerminal's", () => {
     const userLinuxShell = "/some/custom/shell";
     const userWindowsShell = "CustomShell.exe";
     const originalPlatform = process.platform;
+    const newDirectory = "/path/to/directory";
     let userSettings: ICommonSettings;
 
     before(() => {
@@ -36,7 +39,6 @@ describe("IntegratedTerminal's", () => {
     describe("constructor", () => {
         it("attempts to use configured shell when 'useFallbackShell' is false on Linux", () => {
             Object.defineProperty(process, "platform", { value: "linux" });
-
             new IntegratedTerminal(settingsManager.object, shell.object, useFallbackShell);
 
             shell.verify(sh => sh.spawn(userLinuxShell), Times.once());
@@ -44,21 +46,49 @@ describe("IntegratedTerminal's", () => {
 
         it("attempts to use configured shell when 'useFallbackShell' is false on Windows", () => {
             Object.defineProperty(process, "platform", { value: "win32" });
-
             new IntegratedTerminal(settingsManager.object, shell.object, useFallbackShell);
 
             shell.verify(sh => sh.spawn(userWindowsShell), Times.once());
         });
 
-        it("uses fallback shell when 'useFallbackShell' is true");
+        it("uses fallback shell when 'useFallbackShell' is true on Linux", () => {
+            Object.defineProperty(process, "platform", { value: "linux" });
+            useFallbackShell = true;
+            new IntegratedTerminal(settingsManager.object, shell.object, useFallbackShell);
 
-        it("uses cursor blinking if configured to in settings");
+            shell.verify(sh => sh.spawn(applicationSettings.linux.shell), Times.once());
+        });
+
+        it("uses fallback shell when 'useFallbackShell' is true on Windows", () => {
+            Object.defineProperty(process, "platform", { value: "win32" });
+            useFallbackShell = true;
+            new IntegratedTerminal(settingsManager.object, shell.object, useFallbackShell);
+
+            shell.verify(sh => sh.spawn(applicationSettings.windows.shell), Times.once());
+        });
+
+        it("attaches to a constructed Xterm terminal", () => {
+            new IntegratedTerminal(settingsManager.object, shell.object, useFallbackShell);
+
+            shell.verify(sh => sh.attach(It.isAny()), Times.once());
+        });
     });
 
-    describe("shell getter", () => {
-        it("gets the configured shell when 'useFallbackShell' is false");
+    describe("shellName getter", () => {
+        it("gets the configured shell when 'useFallbackShell' is false", () => {
+            Object.defineProperty(process, "platform", { value: "linux" });
+            const terminal = new IntegratedTerminal(settingsManager.object, shell.object, useFallbackShell);
 
-        it("gets the fallback shell if 'useFallbackShell' is true");
+            expect(terminal.shellName).to.equal(userLinuxShell);
+        });
+
+        it("gets the fallback shell if 'useFallbackShell' is true", () => {
+            Object.defineProperty(process, "platform", { value: "linux" });
+            useFallbackShell = true;
+            const terminal = new IntegratedTerminal(settingsManager.object, shell.object, useFallbackShell);
+
+            expect(terminal.shellName).to.equal(applicationSettings.linux.shell);
+        });
     });
 
     describe("fitTo method", () => {
@@ -66,10 +96,26 @@ describe("IntegratedTerminal's", () => {
     });
 
     describe("changeDirectory method", () => {
-        it("begins with a 'cd' command");
+        it("begins with a 'cd' command", () => {
+            const terminal = new IntegratedTerminal(settingsManager.object, shell.object, useFallbackShell);
+            terminal.changeDirectory(newDirectory);
 
-        it("contains the directory to change to, surrounded by single quotes");
+            shell.verify(sh => sh.write(It.is<string>(command => command.startsWith("cd "))), Times.once());
+        });
 
-        it("ends with a os.EOL character to commit the command");
+        it("contains the directory to change to, surrounded by single quotes", () => {
+            const expected = `'${newDirectory}'`;
+            const terminal = new IntegratedTerminal(settingsManager.object, shell.object, useFallbackShell);
+            terminal.changeDirectory(newDirectory);
+
+            shell.verify(sh => sh.write(It.is<string>(command => command.includes(expected))), Times.once());
+        });
+
+        it("ends with a os.EOL character to commit the command", () => {
+            const terminal = new IntegratedTerminal(settingsManager.object, shell.object, useFallbackShell);
+            terminal.changeDirectory(newDirectory);
+
+            shell.verify(sh => sh.write(It.is<string>(command => command.endsWith(os.EOL))), Times.once());
+        });
     });
 });
