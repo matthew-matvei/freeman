2998c5dec9a84c64139862c1d6c5beecd60c6410
Matthew James <matthew.d.james87@gmail.com>
2018-04-07 19:20:24 +0100

    Directory item icon coloured based on accessibility

--- src/renderer/components/blocks/DirectoryItemIcon.tsx
+++ src/renderer/components/blocks/DirectoryItemIcon.tsx
@@ -1,11 +1,12 @@
 import fileIcons from "file-icons-js";
 import * as React from "react";
-import { FaFileO, FaFolderO, FaLock } from "react-icons/lib/fa";
+import { FaFileO, FaFolderO } from "react-icons/lib/fa";
 
 import LoggedError from "errors/LoggedError";
 import { IDirectoryItemIconProps } from "props/blocks";
 
 import "styles/blocks/DirectoryItemIcon.scss";
+import { ItemType } from "types";
 
 /** An icon for a directory item. */
 class DirectoryItemIcon extends React.PureComponent<IDirectoryItemIconProps> {
@@ -24,24 +25,46 @@ class DirectoryItemIcon extends React.PureComponent<IDirectoryItemIconProps> {
         const { directoryItem, directoryItemType, theme } = this.props;
         const { directoryItemIcon } = theme;
 
-        if (directoryItem && !directoryItem.accessible) {
-            return <FaLock
-                style={this.iconStyles}
-                color={directoryItemIcon.inaccessibleIconColour} />;
-        }
-
         if (directoryItem && directoryItem.isDirectory || directoryItemType === "folder") {
-            return <FaFolderO style={this.iconStyles} color={directoryItemIcon.directoryIconColour} />;
+            return <FaFolderO
+                style={this.iconStyles}
+                color={this.getIconColour("folder")} />;
         }
 
         if (directoryItem && !directoryItem.isDirectory || directoryItemType === "file") {
             const className = directoryItem && fileIcons.getClassWithColor(directoryItem.path);
 
-            return className ? <i style={this.iconStyles} className={className}></i> :
-                <FaFileO style={this.iconStyles} color={directoryItemIcon.fileIconDefaultColour} />;
+            return className ?
+                <i style={this.iconStyles}
+                    color={(directoryItem &&
+                        !directoryItem.accessible &&
+                        directoryItemIcon.inaccessibleIconColour) || undefined}
+                    className={className}></i> :
+                <FaFileO
+                    style={this.iconStyles}
+                    color={this.getIconColour("file")} />;
         }
 
         throw new LoggedError("Tried to render directory item icon with no valid prop");
+    }
+
+    /**
+     * Gets the desired colour of the icon.
+     *
+     * @param itemType the type of the directory item
+     *
+     * @returns the desired colour of the icon
+     */
+    private getIconColour(itemType: ItemType): string | undefined {
+        const { directoryItem, theme } = this.props;
+
+        if (itemType === "folder") {
+            return directoryItem && directoryItem.accessible ?
+                theme.directoryItemIcon.directoryIconColour : theme.directoryItemIcon.inaccessibleIconColour;
+        }
+
+        return directoryItem && directoryItem.accessible ?
+            theme.directoryItemIcon.fileIconDefaultColour : theme.directoryItemIcon.inaccessibleIconColour;
     }
 }
 
