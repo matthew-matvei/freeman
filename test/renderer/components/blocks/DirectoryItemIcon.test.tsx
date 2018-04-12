33a6fab8e1f59c51a251ad6c522067cf9654d115
Matthew James <matthew.d.james87@gmail.com>
2018-04-12 21:58:40 +0100

    Implemented directory item icon tests

--- test/renderer/components/blocks/DirectoryItemIcon.test.tsx
+++ test/renderer/components/blocks/DirectoryItemIcon.test.tsx
@@ -3,13 +3,13 @@ import Enzyme, { shallow } from "enzyme";
 import ReactSixteenAdapter from "enzyme-adapter-react-16";
 import fileIcons from "file-icons-js";
 import * as React from "react";
+import { IconBaseProps } from "react-icon-base";
 import { FaFileO, FaFolderO } from "react-icons/lib/fa";
 import "reflect-metadata";
 import Sinon, { SinonSandbox } from "sinon";
 
 import { DirectoryItemIcon } from "components/blocks";
 import { IDirectoryItem } from "models";
-
 import applicationTheme from "settings/internal/themes/dark";
 
 Enzyme.configure({ adapter: new ReactSixteenAdapter() });
@@ -61,6 +61,26 @@ describe("<DirectoryItemIcon />", () => {
         expect(wrapper.findWhere(n => n.type() === "i" && n.hasClass(className))).to.have.length(1);
     });
 
-    it("icon of inaccessible folder is reflected by colour");
-    it("icon of inaccessible file is reflected by colour");
+    it("icon of inaccessible folder is reflected by colour", () => {
+        directoryItem.accessible = false;
+        directoryItem.isDirectory = true;
+        const component = <DirectoryItemIcon directoryItem={directoryItem} theme={applicationTheme} />;
+        const wrapper = shallow(component);
+        const folder = wrapper.find(FaFolderO);
+
+        expect((folder.props() as IconBaseProps).color)
+            .to.equal(applicationTheme.directoryItemIcon.inaccessibleIconColour);
+    });
+
+    it("icon of inaccessible file is reflected by colour", () => {
+        sandbox.stub(fileIcons, "getClassWithColor").returns(null);
+        directoryItem.accessible = false;
+        directoryItem.isDirectory = false;
+        const component = <DirectoryItemIcon directoryItem={directoryItem} theme={applicationTheme} />;
+        const wrapper = shallow(component);
+        const file = wrapper.find(FaFileO);
+
+        expect((file.props() as IconBaseProps).color)
+            .to.equal(applicationTheme.directoryItemIcon.inaccessibleIconColour);
+    });
 });
