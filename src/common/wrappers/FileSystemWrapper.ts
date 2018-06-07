13a941bc9468ee57e028b20291338ef1d52a61d8
Matthew James <matthew.james@zupatech.co.uk>
2018-06-07 11:05:32 +0100

    Made methods readonly

--- src/common/wrappers/FileSystemWrapper.ts
+++ src/common/wrappers/FileSystemWrapper.ts
@@ -8,15 +8,15 @@ import IFileSystemWrapper from "./IFileSystemWrapper";
 @injectable()
 class FileSystemWrapper implements IFileSystemWrapper {
 
-    public lstatAsync = promisify(fs.lstat);
-    public rmdirAsync = promisify(fs.rmdir);
-    public unlinkAsync = promisify(fs.unlink);
-    public renameAsync = promisify(fs.rename);
-    public mkdirAsync = promisify(fs.mkdir);
-    public copyAsync = promisify(ncp.ncp);
-    public writeFileAsync = promisify(fs.writeFile);
-    public accessAsync = promisify(fs.access);
-    public readdirAsync = promisify(fs.readdir);
+    public readonly lstatAsync = promisify(fs.lstat);
+    public readonly rmdirAsync = promisify(fs.rmdir);
+    public readonly unlinkAsync = promisify(fs.unlink);
+    public readonly renameAsync = promisify(fs.rename);
+    public readonly mkdirAsync = promisify(fs.mkdir);
+    public readonly copyAsync = promisify(ncp.ncp);
+    public readonly writeFileAsync = promisify(fs.writeFile);
+    public readonly accessAsync = promisify(fs.access);
+    public readonly readdirAsync = promisify(fs.readdir);
 }
 
 export default FileSystemWrapper;
