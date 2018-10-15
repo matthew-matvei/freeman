79297068e52b8419e18f16d25f8b0cb5c335659c
Matthew James <matthew.d.james87@gmail.com>
2018-10-15 20:17:38 +0100

    Implemented tests

--- test/renderer/components/blocks/InputItem.test.tsx
+++ test/renderer/components/blocks/InputItem.test.tsx
@@ -9,6 +9,7 @@ import { InputItem } from "components/blocks";
 import { IInputItemProps } from "props/blocks";
 import applicationTheme from "settings/internal/themes/dark";
 import { IInputItemState } from "states/blocks";
+import { IDirectoryItem } from 'models';
 
 Enzyme.configure({ adapter: new ReactSixteenAdapter() });
 
@@ -18,17 +19,15 @@ describe("<InputItem />", () => {
 
     let sandbox: SinonSandbox;
 
-    before(() => {
+    beforeEach(() => {
         props = {
             otherItems: [],
             theme: applicationTheme
         };
 
-        sandbox = Sinon.createSandbox();
-    });
-
-    beforeEach(() => {
         component = <InputItem {...props} />;
+
+        sandbox = Sinon.createSandbox();
     });
 
     afterEach(() => {
@@ -63,5 +62,99 @@ describe("<InputItem />", () => {
         expect(renderSpy.callCount).to.equal(1);
         wrapper.setState({ isInvalid: false });
         expect(renderSpy.callCount).to.equal(2);
+    });
+
+    describe("when creating a new item", () => {
+        let sendUpCreateItemStub: Sinon.SinonStub;
+
+        beforeEach(() => {
+            sendUpCreateItemStub = sandbox.stub();
+            props.sendUpCreateItem = sendUpCreateItemStub;
+            component = <InputItem {...props} />;
+        });
+
+        it("always allows the user to cancel item creation", () => {
+            const wrapper = mount(component);
+            const input = wrapper.find("input").first();
+            const event = { key: "Escape" };
+            (input.props() as React.InputHTMLAttributes<HTMLInputElement>)
+                .onKeyUp!(event as React.KeyboardEvent<HTMLInputElement>);
+
+            expect(sendUpCreateItemStub.calledOnce).to.be.true;
+        });
+
+        it("prevents the user creating if the item name is invalid", () => {
+            const wrapper = mount(component);
+            const input = wrapper.find("input").first();
+            (input.instance() as any).value = "";
+            const event = { key: "Enter" };
+            (input.props() as React.InputHTMLAttributes<HTMLInputElement>)
+                .onKeyUp!(event as React.KeyboardEvent<HTMLInputElement>);
+
+            expect(sendUpCreateItemStub.callCount).to.equal(0);
+        });
+
+        it("allows the user to create if the item name is valid", () => {
+            const wrapper = mount(component);
+            const input = wrapper.find("input").first();
+            (input.instance() as any).value = "Some valid input";
+            const event = { key: "Enter" };
+            (input.props() as React.InputHTMLAttributes<HTMLInputElement>)
+                .onKeyUp!(event as React.KeyboardEvent<HTMLInputElement>);
+
+            expect(sendUpCreateItemStub.calledOnce).to.be.true;
+        });
+    });
+
+    describe("when renaming an item", () => {
+        let sendUpRenameItemStub: Sinon.SinonStub;
+        let item: IDirectoryItem = {
+            accessible: true,
+            name: "Some item",
+            path: "/path/to/",
+            isDirectory: false,
+            isHidden: false,
+            lastModified: new Date(),
+            created: new Date()
+        };
+
+        beforeEach(() => {
+            sendUpRenameItemStub = sandbox.stub();
+            props.sendUpRenameItem = sendUpRenameItemStub;
+            props.thisItem = item;
+            component = <InputItem {...props} />;
+        })
+
+        it("always allows the user to cancel item renaming", () => {
+            const wrapper = mount(component);
+            const input = wrapper.find("input").first();
+            const event = { key: "Escape" };
+            (input.props() as React.InputHTMLAttributes<HTMLInputElement>)
+                .onKeyUp!(event as React.KeyboardEvent<HTMLInputElement>);
+
+            expect(sendUpRenameItemStub.calledOnce).to.be.true;
+        });
+
+        it("prevents the user renaming if the item name is invalid", () => {
+            const wrapper = mount(component);
+            const input = wrapper.find("input").first();
+            (input.instance() as any).value = "";
+            const event = { key: "Enter" };
+            (input.props() as React.InputHTMLAttributes<HTMLInputElement>)
+                .onKeyUp!(event as React.KeyboardEvent<HTMLInputElement>);
+
+            expect(sendUpRenameItemStub.callCount).to.equal(0);
+        });
+
+        it("allows the user to rename if the item name is valid", () => {
+            const wrapper = mount(component);
+            const input = wrapper.find("input").first();
+            (input.instance() as any).value = "Some valid input";
+            const event = { key: "Enter" };
+            (input.props() as React.InputHTMLAttributes<HTMLInputElement>)
+                .onKeyUp!(event as React.KeyboardEvent<HTMLInputElement>);
+
+            expect(sendUpRenameItemStub.calledOnce).to.be.true;
+        });
     });
 });
